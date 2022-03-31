d47e48eb3cd39ce6211a77a8394b4ced95c2f461
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-03-31 13:24:18 -0500

    added startQuiz button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var timerEl = document.querySelector(".timer-element")
+var startBtn = document.querySelector(".start-btn")
 
 var questionTwo = document.querySelector(".question-two")
 var questionThree = document.querySelector(".question-three")
@@ -6,23 +7,38 @@ var playerScore = 0;
 
 
 var totalQuestions = [questionTwo, questionThree]
-var timer = 10;
+var timer = 76;
 
-var interval = setInterval(function() {
-    timer--
-    if(timer === 0) {
-       clearInterval(interval)
-       timerEl.textContent = "done";
-       highscores();
-    }
-    else{
-        timerEl.textContent = "Timer: " + timer;
+var startQuiz = function() {
+    var startTimer = setInterval(function() {
+        timer--
+        if(timer === 0) {
+        clearInterval(interval)
+        timerEl.textContent = "done";
+        highscores();
+        }
+        else{
+            timerEl.textContent = "Timer: " + timer;
+            flashQuestions();
 
+        }
+    }, 1000)
+}
+var flashQuestions = function() {
+    for(var i = 0; i < totalQuestions.length; i++) {
+        setTimeout(() => {
+            var x = totalQuestions[i]
+            console.log(x)
+        }, 5000);
     }
-}, 1000)
+}
+
+window.onload=function() {
+    startBtn.addEventListener("click", startQuiz)
+}
 
 var highscores = function() {
-    alert("You finished with " + playerScore + " points!")
+    
 }
 
 // highscore = localStorage.setItem("highscore", playerScore)
