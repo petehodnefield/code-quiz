c6b7ac6f383c7e00ee12a33df67546a26dc55205
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-04-01 10:40:56 -0500

    remade how the project is set up

--- script.js
+++ script.js
@@ -1,41 +1,98 @@
-var timerEl = document.querySelector(".timer-element")
 var startBtn = document.querySelector(".start-btn")
+var nextBtn = document.querySelector(".next-btn")
+var questionContainerEl = document.getElementById("question-container")
 
-var questionTwo = document.querySelector(".question-two")
-var questionThree = document.querySelector(".question-three")
+var randomQuestion, currentQuestionIndex 
+var questionEl = document.getElementById("question")
+var answerButtons = document.getElementById("answer-buttons")
+
+var timerEl = document.querySelector(".timer-element")
 var playerScore = 0;
-
-
-var totalQuestions = [questionTwo, questionThree]
 var timer = 76;
 
-var startQuiz = function() {
-    var startTimer = setInterval(function() {
-        timer--
-        if(timer === 0) {
-        clearInterval(interval)
-        timerEl.textContent = "done";
-        highscores();
-        }
-        else{
-            timerEl.textContent = "Timer: " + timer;
-            flashQuestions();
-
-        }
-    }, 1000)
+var startGame = function() {
+    startBtn.classList.add("hide")
+    randomQuestion = questionList.sort(()=> Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide")
+    nextQuestion()
 }
-var flashQuestions = function() {
-    for(var i = 0; i < totalQuestions.length; i++) {
-        setTimeout(() => {
-            var x = totalQuestions[i]
-            console.log(x)
-        }, 5000);
+
+var nextQuestion = function() {
+    resetState()
+    showQuestion(randomQuestion[currentQuestionIndex])
+}
+
+var showQuestion = function(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", chosenAnswer)
+        answerButtons.appendChild(button)
+    })
+}
+
+var resetState = function() {
+    nextBtn.classList.add("hide")
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild
+        (answerButtons.firstChild)
     }
 }
 
-window.onload=function() {
-    startBtn.addEventListener("click", startQuiz)
+var chosenAnswer = function(e) {
+    var selectedBtn = e.target
+    var correct = selectedBtn.dataset.correct
+    setStatusClass(document.body.correct)
+    Array.from(answerButtons.children)
 }
+
+
+startBtn.addEventListener("click", startGame)
+
+
+var questionList = [
+    {
+        question: "When was JavaScript invented?",
+        answers: [ 
+            {
+                text: "1995",
+                correct: true 
+            },
+            {
+                text: "2020",
+                correct: false
+            },
+            {
+                text: "101",
+                correct: false
+            },
+            {
+                text: "1999",
+                correct: false
+            }
+        ]
+    }
+]
+// var startQuiz = function() {
+    
+// }
+
+
+// var flashQuestions = setInterval(function() {
+//     questionOne.classList.toggle("active-question")
+// }, 5000) 
+    
+
+
+// window.onload=function() {
+//     // startBtn.addEventListener("click", startQuiz)
+// }
 
 var highscores = function() {
     
@@ -50,3 +107,16 @@ var highscores = function() {
 
 // every 5 seconds, set the display of questions to flex
 
+
+var startTimer = setInterval(function() {
+    timer--
+    if(timer === 0) {
+    clearInterval(interval)
+    timerEl.textContent = "done";
+    highscores();
+    }
+    else{
+        timerEl.textContent = "Timer: " + timer;
+        // flashQuestions();
+    }
+}, 1000)
